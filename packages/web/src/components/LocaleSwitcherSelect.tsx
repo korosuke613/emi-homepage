d15113c2a3058cb54ef391a1f8ab7f2146bdc43a
Futa HIRAKOBA <korosuke613613@gmail.com>
2024-07-07 22:15:49 +0900

    feat: setting i18n

---
+++ packages/web/src/components/LocaleSwitcherSelect.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { usePathname, useRouter } from "@/navigation";
+import { useParams } from "next/navigation";
+import { type ChangeEvent, type ReactNode, useTransition } from "react";
+
+type Props = {
+  children: ReactNode;
+  defaultValue: string;
+  label: string;
+};
+
+export default function LocaleSwitcherSelect({
+  children,
+  defaultValue,
+  label,
+}: Props) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const pathname = usePathname();
+  const params = useParams();
+
+  function onSelectChange(event: ChangeEvent<HTMLSelectElement>) {
+    const nextLocale = event.target.value;
+    startTransition(() => {
+      router.replace(
+        // @ts-expect-error -- TypeScript will validate that only known `params`
+        // are used in combination with a given `pathname`. Since the two will
+        // always match for the current route, we can skip runtime checks.
+        { pathname, params },
+        { locale: nextLocale },
+      );
+    });
+  }
+
+  return (
+    <label>
+      <p>{label}</p>
+      <select
+        defaultValue={defaultValue}
+        disabled={isPending}
+        onChange={onSelectChange}
+      >
+        {children}
+      </select>
+      <span>⌄</span>
+    </label>
+  );
+}
