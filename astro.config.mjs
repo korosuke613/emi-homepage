28d82bd27d0b9d67bd23439429c609b98cba798c
Futa HIRAKOBA <korosuke613613@gmail.com>
2024-07-14 23:28:08 +0900

    chore: use vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +9,13 @@ export default defineConfig({
     defaultLocale: 'ja',
     locales: ['ja', 'en']
   },
-  integrations: [react()]
+  integrations: [react()],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+    devImageService: true
+  })
 });
