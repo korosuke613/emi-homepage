cf5a2cc36bb63937a8d6b490b8330706dee9b9f5
Futa HIRAKOBA <korosuke613613@gmail.com>
2024-07-15 05:33:48 +0900

    fix: layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
+import { MoreVert } from "@mui/icons-material";
 import {
   Box,
+  Button,
+  Container,
+  Dropdown,
+  Grid,
+  IconButton,
   Link,
   List,
   ListItem,
   ListItemButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  Sheet,
+  Stack,
   Typography,
+  styled,
 } from "@mui/joy";
 import { type Languages, defaultLang } from "../../i18n/ui";
 import {
@@ -26,8 +38,8 @@ export const Header = ({ lang, path = "/" }: Props) => {
 
   return (
     <header>
-      <Box component="nav" aria-label="My site" sx={{ flexGrow: 1 }}>
-        <List role="menubar" orientation="horizontal">
+      <Box component="nav" aria-label="My site">
+        <List role="menubar" orientation="horizontal" sx={{ padding: "0px" }}>
           <ListItem>
             <Link
               aria-label="to Home"
@@ -37,7 +49,12 @@ export const Header = ({ lang, path = "/" }: Props) => {
               <Typography level="h1">{t("header")}</Typography>
             </Link>
           </ListItem>
-          <ListItem sx={{ marginInlineStart: "auto" }}>
+          <ListItem
+            sx={{
+              marginInlineStart: "auto",
+              display: { xs: "none", sm: "flex" },
+            }}
+          >
             <ListItemButton
               role="menuitem"
               component="a"
@@ -46,7 +63,11 @@ export const Header = ({ lang, path = "/" }: Props) => {
               {t("route.career")}
             </ListItemButton>
           </ListItem>
-          <ListItem>
+          <ListItem
+            sx={{
+              display: { xs: "none", sm: "flex" },
+            }}
+          >
             <ListItemButton
               role="menuitem"
               component="a"
@@ -55,7 +76,64 @@ export const Header = ({ lang, path = "/" }: Props) => {
               {t("route.links")}
             </ListItemButton>
           </ListItem>
-          <ListItem>
+
+          <ListItem
+            sx={{
+              marginInlineStart: { xs: "auto", sm: "0" },
+              display: { xs: "flex", sm: "none" },
+            }}
+          >
+            <ListItemButton
+              role="menuitem"
+              sx={{
+                padding: "unset",
+              }}
+            >
+              <Dropdown>
+                <MenuButton
+                  variant="plain"
+                  sx={{
+                    padding: "unset",
+                    width: "48px",
+                    minHeight: "-webkit-fill-available",
+                    borderRadius: "0%",
+                  }}
+                >
+                  <MoreVert />
+                </MenuButton>
+                <Menu placement="bottom-end">
+                  <MenuItem>
+                    <Link
+                      underline="none"
+                      color="neutral"
+                      href="#horizontal-list"
+                      textColor={"common.black"}
+                    >
+                      {t("route.career")}
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link
+                      underline="none"
+                      color="neutral"
+                      href="#horizontal-list"
+                      textColor={"common.black"}
+                    >
+                      {t("route.links")}
+                    </Link>
+                  </MenuItem>
+                </Menu>
+              </Dropdown>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem
+            sx={{
+              padding: "5px",
+              minHeight: "-webkit-fill-available",
+              borderRadius: "0%",
+            }}
+          >
             <LangSelector path={path} currentLang={lang} />
           </ListItem>
         </List>
