b2416cee40ac063cd286150abb1ac3633fb2d964
Futa HIRAKOBA <korosuke613613@gmail.com>
2024-07-15 03:34:32 +0900

    feat: add Joy UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,65 @@
-import type { Languages } from "../../i18n/ui";
+import {
+  Box,
+  Link,
+  List,
+  ListItem,
+  ListItemButton,
+  Typography,
+} from "@mui/joy";
+import { type Languages, defaultLang } from "../../i18n/ui";
 import {
   useLocalTranslations,
   useLocalTranslationsWithElement,
   useSharedTranslations,
 } from "../../i18n/utils";
+import { LangSelector } from "../LangSelector";
 
 type Props = {
   lang: Languages;
+  path: string;
 };
 
-export const Header = ({ lang }: Props) => {
+export const Header = ({ lang, path = "/" }: Props) => {
   const t = useSharedTranslations(lang);
   const tl = useLocalTranslations(lang);
   const TLE = useLocalTranslationsWithElement(lang);
 
   return (
     <header>
-      <h1>{t("header")}</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="/ja/about">概要</a>
-          </li>
-          <li>
-            <a href="/ja/twitter">ツイッター</a>
-          </li>
-        </ul>
-      </nav>
+      <Box component="nav" aria-label="My site" sx={{ flexGrow: 1 }}>
+        <List role="menubar" orientation="horizontal">
+          <ListItem>
+            <Link
+              aria-label="to Home"
+              underline="none"
+              href={lang === defaultLang ? "/" : `/${lang}`}
+            >
+              <Typography level="h1">{t("header")}</Typography>
+            </Link>
+          </ListItem>
+          <ListItem sx={{ marginInlineStart: "auto" }}>
+            <ListItemButton
+              role="menuitem"
+              component="a"
+              href="#horizontal-list"
+            >
+              {t("route.career")}
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton
+              role="menuitem"
+              component="a"
+              href="#horizontal-list"
+            >
+              {t("route.links")}
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <LangSelector path={path} currentLang={lang} />
+          </ListItem>
+        </List>
+      </Box>
     </header>
   );
 };
--- src/components/LangSelector/LangSelector.stories.tsx
+++ src/components/LangSelector/LangSelector.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { expect, screen, userEvent, within } from "@storybook/test";
 import { LangSelector } from ".";
 
 const metaData: Meta = {
@@ -11,5 +12,55 @@ export default metaData;
 export const Default: StoryObj<typeof LangSelector> = {
   args: {
     path: "/",
+    currentLang: "ja",
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.click(canvas.getByRole("button"));
+
+    const items = screen.getAllByRole("menuitem");
+    for (const item of items) {
+      const link = within(item).getByRole("link");
+      const href = link.getAttribute("href");
+      const linkParent = link.parentElement;
+      const classNames = linkParent?.className.split(" ") ?? [];
+      const ariaLabel = linkParent?.getAttribute("aria-label");
+
+      if (classNames.includes("lang-selector-default")) {
+        expect(href).toBe("/");
+      } else {
+        expect(href).toBe(`/${ariaLabel}`);
+      }
+    }
+  },
+};
+
+export const SubDirectory: StoryObj<typeof LangSelector> = {
+  args: {
+    path: "hoge",
+    currentLang: "ja",
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.click(canvas.getByRole("button"));
+
+    const items = screen.getAllByRole("menuitem");
+    for (const item of items) {
+      const link = within(item).getByRole("link");
+      const href = link.getAttribute("href");
+      const linkParent = link.parentElement;
+      const classNames = linkParent?.className.split(" ") ?? [];
+      const ariaLabel = linkParent?.getAttribute("aria-label");
+
+      if (classNames.includes("lang-selector-default")) {
+        expect(href).toBe("/hoge");
+      } else {
+        expect(href).toBe(`/${ariaLabel}/hoge`);
+      }
+    }
   },
 };
